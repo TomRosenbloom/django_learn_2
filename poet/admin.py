47fa3b5bdd25fe7a7e026809ab679f0058de4eeb
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2018-02-12 23:38:20 +0000

    note about name types/roles in admin

--- poet/admin.py
+++ poet/admin.py
@@ -20,3 +20,16 @@ class PoetAdmin(admin.ModelAdmin):
     inlines = [
         PolynymInline,
     ]
+
+# this works perfectly, but does show up a problem in my models, I think
+# if you choose to add a polynym, you have the a choice of 'role': legal, preferred, pseudonym etc.
+# but these all have the same fields (of course) and some (1) don't really fit (2) would be duplicates
+# e.g. the difference between preferred (or legal, maybe) and maiden name might just be one field the surname
+# e.g. ah wait - more subtle: soubriquet is appearing in the roles list for polynyms
+# when soubriquet is by definition a mononym...
+# hmmm... in https://stackoverflow.com/questions/20958/list-of-standard-lengths-for-database-fields
+# roles are not limitted to certain types of name e.g. for Prince, he has a polynym with
+# a role of legal, but his pictonym also has that role...
+# But, when for e.g. we select to add a Polynym, we don't want soubriquet in the list
+# and when for e.g. we choose to add a mononym, we don't want er... or do we??
+# need to think on this...
