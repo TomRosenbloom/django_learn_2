91e2a13d2d3947a9613451697220bf128939470a
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2018-02-12 23:26:30 +0000

    TabularInline for poet polynym

--- poet/admin.py
+++ poet/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
 from poet.models import Poet
+from person_name.models import Polynym
 
 # Register your models here.
 admin.site.register(Poet)
+admin.site.register(Polynym)
+
+# so... can I add an overiding class in here that will sort out admin
+# for Poet? That is, whereas currently we just see three drop downs for the foreign
+# key rels to Polynymic etc., have those expanded out so to speak, so we can
+# enter a polynymic (or two) for the poet in the Poet admin screen?
+# Sounds a tall order, but...
+
+class PolynymInline(admin.TabularInline):
+    model = Polynym
+
+class PoetAdmin(admin.ModelAdmin):
+    inlines = [
+        PolynymInline,
+    ]
