28fde32493b8ca1a10d8d030edf006018d89da38
Tom Rosenbloom <TomRosenbloom@users.noreply.github.com>
2018-02-27 20:40:25 +0000

    set token to first polynym if no token entered

--- poet/models.py
+++ poet/models.py
@@ -6,6 +6,11 @@ from person.models import Person
 class Poet(Person):
     token = CharField(max_length=50, blank=True, null=True)
 
+    def clean(self):
+        if self.token is None:
+            self.token = '%s %s' % (self.polynym_set.first().given, self.polynym_set.first().surname)
+
+
     def __str__(self):
         #return self.polynym_set.first().surname # rather than first, maybe set one of the roles as default
                                                 # probably legal or preferred?
